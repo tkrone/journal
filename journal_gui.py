e90bb8120409c9ad6be4e6fecd3dffe037e9bcdb
tkslcc <60019174+tkslcc@users.noreply.github.com>
2020-05-26 10:14:26 -0600

    Allowed the button to add the entry text an entered text as a label. Added docstrings.

--- journal_gui.py
+++ journal_gui.py
@@ -2,24 +2,26 @@ import tkinter as tk
 
 
 class Application(tk.Frame):
+    """Represents the GUI of the TO-DO list/journal."""
     def __init__(self, master=None):
+        """Constructor creates the widgets"""
         super().__init__(master)
         self.master = master
         self.pack()
         self.create_widgets()
 
     def create_widgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
+        """Creates and packs the entry field and 'add' button."""
+        self.log_entry = tk.Entry(self, width=35, bg="gray")
+        self.log_entry.pack()
 
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=self.master.destroy)
-        self.quit.pack(side="bottom")
+        self.add_entry = tk.Button(self, text="Add Item", command=self.pack_entry)
+        self.add_entry.pack()
 
-    def say_hi(self):
-        print("hi there, everyone!")
+    def pack_entry(self):
+        """Adds the text in the entry field to the frame after the add_entry button is clicked."""
+        self.item = tk.Label(self, text=self.log_entry.get())
+        self.item.pack()
 
 root = tk.Tk()
 app = Application(master=root)
